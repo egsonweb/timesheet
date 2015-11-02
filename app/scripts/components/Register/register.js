c2bb189894298e44f9709f82debcea5cf7d8a871
Bhargav Anand <rjn143@gmail.com>
2015-11-02 15:16:09 -0800

    Refactor: login/registration styles, add Roboto font

--- app/scripts/components/Register/register.js
+++ app/scripts/components/Register/register.js
@@ -28,7 +28,7 @@ class Register extends Component {
 			address
 		};
 
-		Api.create(formData).then(() => {
+		Api.Registration.post(formData).then(() => {
 			pushState(null, '/register_success/' + formData.email);
 		});
 	}
@@ -36,18 +36,15 @@ class Register extends Component {
 	render() {
 		return (
 			<div className="register">
-				<h2>Register</h2>
 				<form ref="registerForm" className="register-form">
 					<div className="row">
 						<div className="col-sm-6">
 							<div className="form-group">
-						    <label htmlFor="firstname">First Name</label>
 						    <input ref="firstname" type="text" className="form-control" id="firstname" placeholder="First Name" required/>
 						  </div>
 						</div>
 						<div className="col-sm-6">
 							<div className="form-group">
-						    <label htmlFor="lastname">Last Name</label>
 						    <input ref="lastname" type="text" className="form-control" id="lastname" placeholder="Last Name" required/>
 						  </div>
 						</div>
@@ -55,47 +52,40 @@ class Register extends Component {
 					<div className="row">
 						<div className="col-sm-6">
 							<div className="form-group">
-						    <label htmlFor="email">Email address</label>
 						    <input ref="email" type="email" className="form-control" id="email" placeholder="Email" required/>
 						  </div>
 						</div>
 						<div className="col-sm-6">
 						  <div className="form-group">
-						    <label htmlFor="mobile">Mobile</label>
 						    <input ref="mobile" type="tel" className="form-control" id="mobile" placeholder="Mobile" required/>
 						  </div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-6">
+						<div className="col-sm-4">
 							<div className="form-group">
-								<label htmlFor="apt">Apartment</label>
 								<input ref="apt" type="text" className="form-control" id="apt" placeholder="Apt #111" required/>
 							</div>
 						</div>
-						<div className="col-sm-6">
+						<div className="col-sm-8">
 							<div className="form-group">
-								<label htmlFor="street">Street</label>
 								<input ref="street" type="text" className="form-control" id="street" placeholder="Street" required/>
 							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-5">
 							<div className="form-group">
-								<label htmlFor="city">City</label>
 								<input ref="city" type="text" className="form-control" id="city" placeholder="City" required/>
 							</div>
 						</div>
 						<div className="col-sm-4">
 							<div className="form-group">
-								<label htmlFor="state">State</label>
 								<input ref="state" type="text" className="form-control" id="state" placeholder="State" required/>
 							</div>
 						</div>
-						<div className="col-sm-4">
+						<div className="col-sm-3">
 							<div className="form-group">
-								<label htmlFor="zipcode">Zipcode</label>
 								<input ref="zipcode" type="text" className="form-control" id="zipcode" placeholder="Zipcode" required/>
 							</div>
 						</div>
